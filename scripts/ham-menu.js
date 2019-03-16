73b2056e0c460ba0d2da4b7472466bae8923b4d9
bergmaner <bergkacper@gmail.com>
2019-03-16 23:33:48 +0100

    change a menu button

--- scripts/ham-menu.js
+++ scripts/ham-menu.js
@@ -5,15 +5,19 @@ wrapper.addEventListener("click",function(){
 
     if(wrapper.classList.contains("active"))
     {
-        container.removeChild(document.querySelector(".menu-panel"));
+        container.removeChild(document.querySelector(".menu-panel"));        
+        $(".menu").css("background-color","#bbb");
+        $(".hamburger span").css("background-color","#333");
     }
     else{
         const menu=document.createElement("div");
         menu.classList.add("menu-panel");
         console.log(menu);
         container.appendChild(menu);
+        $(".menu").css("background-color","#222");
+        $(".hamburger span").css("background-color","#bbb");
+     
     }
-
 
 this.classList.toggle("active");
 });
