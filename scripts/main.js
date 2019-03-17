f77b2f13e3a9df7bb36bdcaf86f9275c249a0cfc
bergmaner <bergkacper@gmail.com>
2019-03-17 13:10:12 +0100

    i am gonna make better optimalisation

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,36 @@
 const panels = $(".headeranim");
 const kacperheader = $(".kheader");
 const patrickheader = $(".pheader");
-
-$(patrickheader).mouseenter(function () {
-  $(patrickheader).css("color", "rgb(39, 39, 39)");
-  $(patrickheader).css("cursor", "pointer");
-  $(patrickheader).css("text-shadow", "none");
-  $(patrickheader).css("transform", "translate(1vw, -.5vw)");
-  $("#headeranimleft").css("transform", "translate(1vw, -.5vw)");
-  $("#headeranimleft").css("width", "60%");
-  $("#headeranimleft").css("box-shadow", ".15vw .15vw black");
-
-}).mouseleave(function () {
-  $(patrickheader).css("color", "white");
-  $(patrickheader).css("text-shadow", ".12vw .12vw black");
-  $(patrickheader).css("transform", "translate(0)");
-  $("#headeranimleft").css("transform", "translate(0)");
-  $("#headeranimleft").css("width", "0");
-  $("#headeranimleft").css("box-shadow", "none");
-});
-function enterdata()
+function menter(object,anim)
 {
+  object.css("color", "rgb(39, 39, 39)");
+  object.css("cursor", "pointer");
+  object.css("text-shadow", "none");
+  object.css("transform", "translate(1vw, -.5vw)");
   
+ anim.css("transform", "translate(1vw, -.5vw)");
+ anim.css("width", "60%");
+ anim.css("box-shadow", ".15vw .15vw black");
 }
-kacperheader.mouseenter(function () {
- 
-  kacperheader.css("color", "rgb(39, 39, 39)");
-  kacperheader.css("cursor", "pointer");
-  kacperheader.css("text-shadow", "none");
-  kacperheader.css("transform", "translate(1vw, -.5vw)");
-  $("#headeranimright").css("transform", "translate(1vw, -.5vw)");
-  $("#headeranimright").css("width", "60%");
-  $("#headeranimright").css("box-shadow", ".15vw .15vw black");
+function mleave(object,anim)
+{
+  object.css("color", "white");
+  object.css("text-shadow", ".12vw .12vw black");
+  object.css("transform", "translate(0)");
+  anim.css("transform", "translate(0)");
+  anim.css("width", "0");
+  anim.css("box-shadow", "none");
+}
+$(patrickheader).mouseenter(function () {
+  menter(patrickheader,$("#headeranimleft"));
+
 }).mouseleave(function () {
-  kacperheader.css("color", "white");
-  kacperheader.css("text-shadow", ".12vw .12vw black");
-  kacperheader.css("transform", "translate(0)");
-  $("#headeranimright").css("transform", "translate(0)");
-  $("#headeranimright").css("width", "0");
-  $("#headeranimright").css("box-shadow", "none");
+  mleave(patrickheader,$("#headeranimleft"));
+});
+
+kacperheader.mouseenter(function () {
+ menter(kacperheader,$("#headeranimright"));
+ 
+}).mouseleave(function () {
+  mleave(kacperheader,$("#headeranimright"));
 });
