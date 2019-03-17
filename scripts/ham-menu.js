db0e71b4fba605f1ec134dcd93f58064c4fc912a
bergmaner <bergkacper@gmail.com>
2019-03-17 11:09:12 +0100

    navigation

--- scripts/ham-menu.js
+++ scripts/ham-menu.js
@@ -2,9 +2,7 @@ const mainmenu=document.querySelector(".menu");
 const wrapper=document.querySelector(".hamburger");
 const container=document.querySelector(".maincontainer");
 
-$("document").ready(function(){
-    $(".menu-panel").slideToggle();
-});
+$("document").ready(function(){$(".menu-panel").slideToggle();} );
 mainmenu.addEventListener("click",function(){
 
     if(wrapper.classList.contains("active"))
@@ -13,8 +11,9 @@ mainmenu.addEventListener("click",function(){
         // container.removeChild(document.querySelector(".menu-panel"));        
         $(".menu").css("background-color","#bbb");
         $(".hamburger span").css("background-color","#222");
-        $(".menu").css("border-color","#333333")
-        $(".menu").css("box-shadow","0 0 0 2px #bbb")
+        $(".menu").css("border-color","#333333");
+        $(".menu").css("box-shadow","0 0 0 2px #bbb");
+        
        
     }
     else{
@@ -23,10 +22,11 @@ mainmenu.addEventListener("click",function(){
         // container.appendChild(menu);
         $(".menu").css("background-color","#222");
         $(".hamburger span").css("background-color","#bbb");
-        $(".menu").css("border-color","#bbbbbb")
-        $(".menu").css("box-shadow","0 0 0 2px #222")
-     
+        $(".menu").css("border-color","#bbbbbb");
+        $(".menu").css("box-shadow","0 0 0 2px #222");
+        
     }
+    $(".menu-panel").css("opacity","1");
     $(".menu-panel").slideToggle();
 wrapper.classList.toggle("active");
 });
